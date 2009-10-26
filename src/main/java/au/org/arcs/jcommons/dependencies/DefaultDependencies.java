effa7bddfea312c82e2a1de0ac02b25e26c1e824
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-10-26 13:29:15 +1100

    Change download urls.

--- src/main/java/au/org/arcs/jcommons/dependencies/DefaultDependencies.java
+++ src/main/java/au/org/arcs/jcommons/dependencies/DefaultDependencies.java
@@ -6,8 +6,8 @@ public enum DefaultDependencies {
 	LOCALSERVICEINTERFACE("org.vpac.grisu.control.serviceInterfaces.LocalServiceInterface", "https://code.arcs.org.au/hudson/job/Grisu-SNAPSHOT/org.vpac.grisu$grisu-core/lastSuccessfulBuild/artifact/org.vpac.grisu/grisu-core/0.3-SNAPSHOT/local-backend.jar"),
 	XFIRESERVICEINTERFACECREATOR("org.vpac.grisu.client.control.XFireServiceInterfaceCreator", "https://code.arcs.org.au/hudson/job/Grisu-connectors-SNAPSHOT-binaries/lastSuccessfulBuild/artifact/frontend-modules/xfire-frontend/target/xfire-frontend.jar"),
 	CLIENTSIDEMDS("org.vpac.grisu.frontend.info.clientsidemds.ClientSideGrisuRegistry", "https://code.arcs.org.au/hudson/job/Grisu-SNAPSHOT-binaries/lastSuccessfulBuild/artifact/frontend/client-side-mds/target/client-side-mds.jar"),
-	ARCSGSI("au.org.arcs.auth.slcs.ArcsGsiPackageIndicator", "http://code.arcs.org.au/nexus/content/repositories/snapshots/au/org/arcs/auth/arcs-gsi/${version}/arcs-gsi-${version}-lib.jar"),
-	ARCSGSI_WITHOUTPYTHON("au.org.arcs.auth.slcs.ArcsGsiPackageIndicator", "https://code.arcs.org.au/nexus/content/repositories/snapshots/au/org/arcs/auth/arcs-gsi/1.1-SNAPSHOT/arcs-gsi-1.1-SNAPSHOT-lib-without-jython.jar"),
+	ARCSGSI("au.org.arcs.auth.slcs.ArcsGsiPackageIndicator", "https://code.arcs.org.au/nexus/content/repositories/releases/au/org/arcs/auth/arcs-gsi/${version}/arcs-gsi-${version}-lib.jar"),
+	ARCSGSI_WITHOUTPYTHON("au.org.arcs.auth.slcs.ArcsGsiPackageIndicator", "https://code.arcs.org.au/nexus/content/repositories/releases/au/org/arcs/auth/arcs-gsi/${version}/arcs-gsi-${version}-lib-without-jython.jar"),
 	BOUNCYCASTLE("org.bouncycastle.jce.provider.BouncyCastleProvider", "http://www.bouncycastle.org/download/bcprov-jdk15-143.jar");
 	
 	private String className;
