5eb79dad16a363d3815c8ed976748d5de166b123
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-10-30 09:26:10 +1100

    Improved dependency management.

--- src/main/java/au/org/arcs/jcommons/dependencies/Dependency.java
+++ src/main/java/au/org/arcs/jcommons/dependencies/Dependency.java
@@ -8,12 +8,15 @@ public enum Dependency {
 //	LOCALSERVICEINTERFACE("https://code.arcs.org.au/hudson/job/Grisu-SNAPSHOT/org.vpac.grisu$grisu-core/lastSuccessfulBuild/artifact/org.vpac.grisu/grisu-core/0.3-SNAPSHOT/local-backend.jar"),
 //	XFIRESERVICEINTERFACECREATOR("https://code.arcs.org.au/hudson/job/Grisu-connectors-SNAPSHOT-binaries/lastSuccessfulBuild/artifact/frontend-modules/xfire-frontend/target/xfire-frontend.jar"),
 //	CLIENTSIDEMDS("https://code.arcs.org.au/hudson/job/Grisu-SNAPSHOT-binaries/lastSuccessfulBuild/artifact/frontend/client-side-mds/target/client-side-mds.jar"),
-	ARCSGSI("http://code.arcs.org.au/nexus/content/repositories/releases/au/org/arcs/auth/arcs-gsi/${version}", "arcs-gsi-${version}-lib.jar"),
-	ARCSGSI_SNAPSHOT("http://code.arcs.org.au/nexus/content/repositories/snapshots/au/org/arcs/auth/arcs-gsi/${version}", "arcs-gsi-${version}-lib.jar"),
-	ARCSGSI_WITHOUTPYTHON("http://code.arcs.org.au/nexus/content/repositories/releases/au/org/arcs/auth/arcs-gsi/${version}", "arcs-gsi-${version}-lib-without-jython.jar"),
-	ARCSGSI_WITHOUTPYTHON_SNAPSHOT("http://code.arcs.org.au/nexus/content/repositories/snapshots/au/org/arcs/auth/arcs-gsi/${version}", "arcs-gsi-${version}-lib-without-jython.jar"),
+	ARCSGSI("${repository}/au/org/arcs/auth/arcs-gsi/${version}", "arcs-gsi-${version}-lib.jar"),
+	ARCSGSI_WITHOUTPYTHON("${repository}/au/org/arcs/auth/arcs-gsi/${version}", "arcs-gsi-${version}-lib-without-jython.jar"),
 	BOUNCYCASTLE("http://www.bouncycastle.org/download/", "bcprov-${version}.jar"), 
-	GRISU_LOCAL_BACKEND("http://code.arcs.org.au/nexus/content/repositories/snapshots/org/vpac/grisu/grisu-core/${version}", "grisu-core-${version}-backend.jar");
+	GRISU_LOCAL_BACKEND("${repository}/org/vpac/grisu/grisu-core/${version}", "grisu-core-${version}-backend.jar"), 
+	GRISU_XFIRE_CLIENT_LIBS("$repository}/org/vpac/grisu/grisu-client-xfire/${version}", "grisu-client-xfire-${version}.jar"),
+	CLIENT_SIDE_MDS("${repository}/org/vpac/grisu/client-side-mds/${version}", "client-side-mds-${version}-side-mds.jar");
+	
+	public static final String RELEASES_DEFAULT_URL = "http://code.arcs.org.au/nexus/content/repositories/releases";
+	public static final String SNAPSHOTS_DEFAULT_URL = "http://code.arcs.org.au/nexus/content/repositories/snapshots";
 	
 	private String filename;
 	private String downloadUrl;
@@ -30,7 +33,14 @@ public enum Dependency {
 	
 	public String getDownloadUrl(String version) {
 		
-		return this.downloadUrl.replace("${version}", version) + "/" + getFileName(version);
+		String temp = this.downloadUrl.replace("${version}", version) + "/" + getFileName(version);
+		
+		if ( version.indexOf("SNAPSHOT") >= 0) {
+			temp = temp.replace("${repository}", SNAPSHOTS_DEFAULT_URL);
+		} else {
+			temp = temp.replace("${repository}", RELEASES_DEFAULT_URL);
+		}
+		return temp;
 	}
 	
 	public File getDependencyFile(String version, File folder) {
