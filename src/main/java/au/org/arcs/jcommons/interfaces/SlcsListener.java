8f1880b75f1a75f251ac9a8bdc45aa8bb4ed61e7
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-10-29 17:41:04 +1100

    Moved slcsListener interface into commons package.

---
+++ src/main/java/au/org/arcs/jcommons/interfaces/SlcsListener.java
@@ -0,0 +1,12 @@
+package au.org.arcs.jcommons.interfaces;
+
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+
+public interface SlcsListener {
+	
+	public void slcsLoginComplete(X509Certificate cert, PrivateKey privateKey);
+	
+	public void slcsLoginFailed(String message, Exception optionalException);
+
+}
