b9b6a57c8c0cfb1bd8a2f2f86af5c968a1b1c93d
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-08-18 12:15:49 +0200

    Added prettyName to jobsubmissionproperty enums

--- src/main/java/grisu/jcommons/constants/JobSubmissionProperty.java
+++ src/main/java/grisu/jcommons/constants/JobSubmissionProperty.java
@@ -19,7 +19,7 @@ public enum JobSubmissionProperty {
 	 * for this job, specify the value of
 	 * {@link Constants#GENERIC_APPLICATION_NAME} here. Defaults to null.
 	 */
-	APPLICATIONNAME(Constants.APPLICATIONNAME_KEY, ""),
+	APPLICATIONNAME(Constants.APPLICATIONNAME_KEY, "package", ""),
 	/**
 	 * The version of the application for this job. If you want the Grisu
 	 * backend to pick a version, specify the value of
@@ -27,6 +27,7 @@ public enum JobSubmissionProperty {
 	 */
 	APPLICATIONVERSION(
 			Constants.APPLICATIONVERSION_KEY,
+			"version",
 			Constants.NO_VERSION_INDICATOR_STRING),
 			/**
 			 * The numbers of cpus for this job. If you don't specify
@@ -38,12 +39,12 @@ public enum JobSubmissionProperty {
 			 * Forces a "single" type job, even if you specify more than one cpus in
 			 * {@link #NO_CPUS}. Defaults to false.
 			 */
-			FORCE_SINGLE(Constants.FORCE_SINGLE_KEY, "false"),
+			FORCE_SINGLE(Constants.FORCE_SINGLE_KEY, "single", "false"),
 			/**
 			 * Forces a "mpi" type job, even if you specify only one cpu in
 			 * {@link #NO_CPUS}. Defaults to false
 			 */
-			FORCE_MPI(Constants.FORCE_MPI_KEY, "false"),
+			FORCE_MPI(Constants.FORCE_MPI_KEY, "mpi", "false"),
 			/**
 			 * The minimum amount of memory that this job needs. Defaults to 0.
 			 */
@@ -52,7 +53,7 @@ public enum JobSubmissionProperty {
 			 * The email address to use when specifying {@link #EMAIL_ON_START} or
 			 * {@link #EMAIL_ON_FINISH}.
 			 */
-			EMAIL_ADDRESS(Constants.EMAIL_ADDRESS_KEY, ""),
+			EMAIL_ADDRESS(Constants.EMAIL_ADDRESS_KEY, "email", ""),
 			/**
 			 * Specifies whether you want an email sent to you after the job started on
 			 * the cluster. Defaults to false.
@@ -95,7 +96,7 @@ public enum JobSubmissionProperty {
 			 * A comma seperated list of urls of input files. You can specify local or
 			 * remote files here. Defaults to null.
 			 */
-			INPUT_FILE_URLS(Constants.INPUT_FILE_URLS_KEY, ""),
+			INPUT_FILE_URLS(Constants.INPUT_FILE_URLS_KEY, "files", ""),
 			/**
 			 * A comma-seperated list of modules you want to load. This should only be
 			 * used in if you know what you are doing. Defaults to null.
@@ -106,7 +107,7 @@ public enum JobSubmissionProperty {
 			/**
 			 * The number of hosts to spread the job. Only important for threaded jobs.
 			 */
-			HOSTCOUNT(Constants.HOSTCOUNT_KEY, "-1"),
+			HOSTCOUNT(Constants.HOSTCOUNT_KEY, "hostcount", "-1"),
 			/**
 			 * List of keys and values that describe the environment variables to be set
 			 * for a job (format: [key1=value1][key2=value2]...)
@@ -120,6 +121,24 @@ public enum JobSubmissionProperty {
 		}
 	}
 
+	private static final Map<String, JobSubmissionProperty> prettyNameToJobPropertyMap = new HashMap<String, JobSubmissionProperty>();
+	static {
+		for (JobSubmissionProperty jp : values()) {
+			prettyNameToJobPropertyMap.put(jp.toString(), jp);
+		}
+	}
+
+	/**
+	 * Translates a pretty name string to a JobSubmissionProperty enum.
+	 * 
+	 * @param key
+	 *            the pretty name
+	 * @return the enum
+	 */
+	public static JobSubmissionProperty fromPrettyName(final String pn) {
+		return prettyNameToJobPropertyMap.get(pn);
+	}
+
 	/**
 	 * Translates a string to a JobSubmissionProperty enum.
 	 * 
@@ -131,8 +150,17 @@ public enum JobSubmissionProperty {
 		return stringToJobPropertyMap.get(key);
 	}
 
-	private final String keyName;
+	public static String getPrettyName(final String key) {
+		JobSubmissionProperty p = fromString(key);
+		if (p == null) {
+			return null;
+		} else {
+			return p.prettyName;
+		}
+	}
 
+	private final String keyName;
+	private final String prettyName;
 	private final String defaultValue;
 
 	/**
@@ -145,6 +173,24 @@ public enum JobSubmissionProperty {
 	 */
 	JobSubmissionProperty(final String keyName, final String defaultValue) {
 		this.keyName = keyName;
+		this.prettyName = keyName;
+		this.defaultValue = defaultValue;
+	}
+
+	/**
+	 * Constructor for a JobSubmissionProperty enum.
+	 * 
+	 * @param keyName
+	 *            the name of the property
+	 * @param prettyName
+	 *            the name to display to users for this job property
+	 * @param defaultValue
+	 *            the default value
+	 */
+	JobSubmissionProperty(final String keyName, final String prettyName,
+			final String defaultValue) {
+		this.keyName = keyName;
+		this.prettyName = prettyName;
 		this.defaultValue = defaultValue;
 	}
 
