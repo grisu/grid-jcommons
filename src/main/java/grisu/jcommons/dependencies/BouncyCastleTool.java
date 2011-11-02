d88615359ae8613f1d5dfc40e48c34c58e1245e4
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-11-03 11:04:44 +1300

    Added class to deal with bouncycastle class loading.

---
+++ src/main/java/grisu/jcommons/dependencies/BouncyCastleTool.java
@@ -0,0 +1,110 @@
+package grisu.jcommons.dependencies;
+
+import grisu.jcommons.constants.GridEnvironment;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.security.Provider;
+import java.security.Security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BouncyCastleTool {
+
+	private static Logger myLogger = LoggerFactory
+			.getLogger(BouncyCastleTool.class);
+
+	private static final String[] possiblePaths = new String[] {
+		BouncyCastleTool.class.getProtectionDomain().getCodeSource()
+		.getLocation().getPath(),
+		GridEnvironment.getGridCommonJavaLibDirectory().getAbsolutePath(),
+	"/usr/share/java" };
+
+	private static boolean addExternalBouncyCastle() {
+
+
+		for (String path : possiblePaths) {
+
+			myLogger.debug("Looking for bouncy-castle library in: {}", path);
+
+			File dir = new File(path);
+			if (!dir.exists() || !dir.canRead()) {
+				continue;
+			}
+
+			String[] files = dir.list(new FilenameFilter() {
+
+				public boolean accept(File arg0, String arg1) {
+					File file = new File(arg0, arg1);
+					if (file.isFile() && file.canRead()
+							&& arg1.startsWith("bcprov")) {
+						return true;
+					} else {
+						return false;
+					}
+				}
+			});
+
+			for (String file : files) {
+				try {
+					ClasspathHacker.addFile(new File(dir, file));
+					myLogger.debug("Loaded bouncy castle from: {}", file);
+					return true;
+				} catch (IOException e) {
+					myLogger.debug("Could not load: {}", file);
+				}
+
+			}
+		}
+		myLogger.error("Could not find/load bouncy castle provider...");
+		return false;
+	}
+
+	public static int initBouncyCastle() throws ClassNotFoundException {
+
+		// System.out.println("SimpleProxyLib updated");
+
+		// try {
+		Class bcClass = null;
+		try {
+			bcClass = Class
+					.forName("org.bouncycastle.jce.provider.BouncyCastleProvider");
+		} catch (ClassNotFoundException e1) {
+			BouncyCastleTool.addExternalBouncyCastle();
+			bcClass = bcClass
+					.forName("org.bouncycastle.jce.provider.BouncyCastleProvider");
+		}
+
+		final Class temp = bcClass;
+		AccessController.doPrivileged(new PrivilegedAction<Void>() {
+			public Void run() {
+
+				try {
+
+					// bouncy castle
+					if (Security.addProvider((Provider) temp.newInstance()) == -1) {
+						myLogger.debug("Could not add BouncyCastleProvider because it is already installed.");
+					}
+					return null;
+				} catch (Throwable e) {
+					// e.printStackTrace();
+					myLogger.error("Could not load BouncyCastleProvider.", e);
+					return null;
+				}
+			}
+		});
+		// } catch (Throwable e) {
+		// // e.printStackTrace();
+		// myLogger.error("Could not load BouncyCastleProvider.", e);
+		// // throw new RuntimeException(e);
+		// return -1;
+		// }
+		myLogger.info("Loaded BouncyCastleProvider.");
+		return 0;
+	}
+
+}
