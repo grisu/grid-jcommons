c139c4273ba94b894f06b0dd896806e6b4d15918
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-01-05 16:49:16 +1300

    Added stringhelper class.

---
+++ src/main/java/grisu/jcommons/utils/VariousStringHelpers.java
@@ -0,0 +1,21 @@
+package grisu.jcommons.utils;
+
+import org.apache.commons.lang.StringUtils;
+
+public class VariousStringHelpers {
+
+	public static String getCN(String dn) {
+
+		if (StringUtils.isBlank(dn)) {
+			return "";
+		}
+
+		int index = dn.lastIndexOf("=");
+		if (index < 0) {
+			index = 0;
+		}
+		return dn.substring(index);
+
+	}
+
+}
