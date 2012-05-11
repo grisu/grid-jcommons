ddf0e2f94e5ed88bca03f01df1b3b5b5b759ae0a
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-05-11 17:38:06 +1200

    Added extre constructor.

--- src/main/java/grisu/model/info/dto/FileSystem.java
+++ src/main/java/grisu/model/info/dto/FileSystem.java
@@ -1,18 +1,50 @@
 package grisu.model.info.dto;
 
+import grisu.jcommons.utils.FileSystemHelpers;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import org.apache.commons.lang.StringUtils;
 
 @XmlRootElement(name = "filesystem")
 public class FileSystem {
 
 	public static final int DEFAULT_PORT = 2811;
+	public static final String DEFAULT_PROTOCOL = "gsiftp";
 
 	private String host;
 
 	private Site site;
 	private int port;
 	private String protocol;
+
+	public FileSystem() {
+	}
+
+	public FileSystem(String url) {
+		setHost(FileSystemHelpers.getHost(url));
+		int temp = -1;
+		try {
+			temp = FileSystemHelpers.getPort(url);
+		} catch (Exception e) {
+			temp = DEFAULT_PORT;
+		}
+
+		if (temp < 0) {
+			setPort(DEFAULT_PORT);
+		} else {
+			setPort(temp);
+		}
+
+		String tempP = FileSystemHelpers.getProtocol(url);
+		if (StringUtils.isBlank(tempP)) {
+			setProtocol(DEFAULT_PROTOCOL);
+		} else {
+			setProtocol(tempP);
+		}
+
+	}
 
 	@XmlElement(name = "host")
 	public String getHost() {
